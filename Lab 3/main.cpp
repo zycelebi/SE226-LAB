ff8f64e544e2d843f6bae75ee1a010e9d382e37a
Zeynep Çelebi <153827591+zycelebi@users.noreply.github.com>
2026-03-12 13:22:54 +0300

    Update main.cpp

--- Lab 3/main.cpp
+++ Lab 3/main.cpp
@@ -1,2 +1,99 @@
+#include <iostream>
+using namespace std;
+void swapValues(int* p1, int* p2) {
+    int temp = *p1;
+    *p1 = *p2;
+    *p2 = temp;
+}
+
+void printArray(int* arr, int size) {
+    for(int i = 0; i < size; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int findSum(int* arr, int size) {
+    int sum = 0;
+
+    for(int i = 0; i < size; i++) {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}
+
+void shiftRight(int* arr, int size) {
+
+    int last = arr[size - 1];
+
+    for(int i = size - 1; i > 0; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[0] = last;
+}
+
+int* createArray(int size) {
+    int* arr = new int[size];
+    return arr;
+}
+
+void deleteArray(int* arr) {
+    delete[] arr;
+}
+int main() {
+
+    cout << "Creating dynamic array..." << endl;
+
+    int size;
+    cout << "Enter array size: ";
+    cin >> size;
+
+    int* arr = createArray(size);
+
+    cout << "Enter values: ";
+    for(int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Array elements:" << endl;
+    printArray(arr, size);
+
+    cout << "Sum of elements: " << findSum(arr, size) << endl;
+
+    cout << "----------------------------------" << endl;
+
+    int a = 5, b = 8;
+
+    cout << "Swapping two numbers" << endl;
+    cout << "Before swap" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    swapValues(&a, &b);
+
+    cout << "After swap" << endl;
+    cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+
+    cout << "----------------------------------" << endl;
+
+    cout << "Shifting array to the right..." << endl;
+    shiftRight(arr, size);
+
+    cout << "Array after shiftRight:" << endl;
+    printArray(arr, size);
+
+    cout << "----------------------------------" << endl;
+
+    cout << "Deleting array..." << endl;
+    deleteArray(arr);
+
+    cout << "Memory released successfully." << endl;
+
+    return 0;
+}
+
 
 
